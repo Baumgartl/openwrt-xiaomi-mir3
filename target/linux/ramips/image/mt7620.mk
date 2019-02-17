220e475bf976617e2e3ce92af8bfac6b2a501583
Baumgartl <florian@baumgartl.net>
2019-02-17 22:00:53 +0100

    mt7620: image.mk: update target xiaomi_miwifi-r3

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -590,12 +590,10 @@ define Device/xiaomi_miwifi-r3
   KERNEL := $(KERNEL_DTB) | uImage lzma
   IMAGE_SIZE := 32768k
   UBINIZE_OPTS := -E 5
-  IMAGES := kernel1.bin rootfs0.bin sysupgrade.tar pb-boot.bin
+  IMAGES := sysupgrade.tar kernel1.bin rootfs0.bin
   IMAGE/kernel1.bin := append-kernel | check-size $$$$(KERNEL_SIZE)
   IMAGE/rootfs0.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
-  IMAGE/pb-boot.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-kernel | \
-	pad-to $$(KERNEL_SIZE) | append-ubi | check-size $$$$(IMAGE_SIZE)
   DEVICE_TITLE := Xiaomi Mi Router R3
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-mt76x2 uboot-envtools
 endef
